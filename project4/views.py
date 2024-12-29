74b98da3b9d8abebd517ae27ddb5321de7e9d216
Megan Saenz <saenzmegan@ymail.com>
2024-12-29 18:48:15 +0000

    Manually export URL

--- project4/views.py
+++ project4/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 4 is going to be so fun!")
+    return HttpResponse("Project 4 is going to be so fun and I'm going to learn so much!")

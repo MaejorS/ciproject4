f7e542af42f2ebee8ef2c21e53ce78b9c0040347
Megan Saenz <saenzmegan@ymail.com>
2025-01-12 14:45:22 +0000

    Change Debug to False for manual deployment to Heroku

--- my_project4/urls.py
+++ my_project4/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from project4 import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("project4.urls"), name="project4-urls"),
     path('admin/', admin.site.urls),
 ]

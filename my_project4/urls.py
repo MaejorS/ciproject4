3e67b2b1380019194b9b4763eaf0da788eb3c071
Megan Saenz <saenzmegan@ymail.com>
2025-01-18 16:23:43 +0000

    Install Django AllAuth, configure AllAuth library, connect and style login/logout pages

--- my_project4/urls.py
+++ my_project4/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("project4.urls"), name="project4-urls"),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 ]

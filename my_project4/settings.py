a15b86df33dcc5fb4155f909c6ecc6c6b8b4ec1f
Megan Saenz <saenzmegan@ymail.com>
2025-01-18 17:04:34 +0000

    Remove a tester allowed host

--- my_project4/settings.py
+++ my_project4/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-maejors-ciproject4-rsws6gvx7sa.ws.codeinstitute-ide.net', '.herokuapp.com', 'ci-project4.herokapp.com']
+ALLOWED_HOSTS = ['8000-maejors-ciproject4-rsws6gvx7sa.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
 
 # Application definition

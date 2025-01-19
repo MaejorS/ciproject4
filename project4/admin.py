5a89b87b696a781511129c5642101211d1f18bee
Megan Saenz <saenzmegan@ymail.com>
2025-01-19 19:47:12 +0000

    Register Subscriber model to admin.py

--- project4/admin.py
+++ project4/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Subscriber
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -11,6 +11,11 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'is_active', 'subscribed_on')
+    search_fields = ('email', 'name')
+    list_filter = ('is_active', 'subscribed_on')
 
 # Register your models here.
 admin.site.register(Comment)

095cdbe4fe00a6d21d876dd5580f8598919bde65
Megan Saenz <saenzmegan@ymail.com>
2025-01-19 20:06:41 +0000

    Add URL pattern for the subscribe view in project4 urls.py

--- project4/urls.py
+++ project4/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
+    path("subscribe/", views.subscribe, name="subscribe")
 ]

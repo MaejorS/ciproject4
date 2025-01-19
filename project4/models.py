61432310f1293a60274f6cadbe926b46c66e6133
Megan Saenz <saenzmegan@ymail.com>
2025-01-19 19:34:27 +0000

    Construct Subscriber model to models.py

--- project4/models.py
+++ project4/models.py
@@ -35,3 +35,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} | posted by {self.author}"
+
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, blank=True)
+    subscribed_on = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ["-subscribed_on"]
+
+    def __str__(self):
+        return f"{self.name or 'Anonymous'} ({self.email})"

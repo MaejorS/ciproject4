4d33d7357689d4ca26a69022c6683e2d20b5e0e8
Megan Saenz <saenzmegan@ymail.com>
2025-01-19 14:13:49 +0000

    Reorder url patterns so that admin site loads properly

--- my_project4/urls.py
+++ my_project4/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("project4.urls"), name="project4-urls"),
+    path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("project4.urls"), name="project4-urls"),
 ]

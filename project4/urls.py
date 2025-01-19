a956b2f6339e4aea23a61d2dc2ba166a644f7084
Megan Saenz <saenzmegan@ymail.com>
2025-01-19 20:28:31 +0000

    Reorder URL patterns so subscribe loads first and does not produce error

--- project4/urls.py
+++ project4/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path("subscribe/", views.subscribe, name="subscribe"),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
-    path("subscribe/", views.subscribe, name="subscribe")
 ]

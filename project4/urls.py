f15857ad8f23a06e0b660f1f2673ea2f1dcadda5
Megan Saenz <saenzmegan@ymail.com>
2025-01-22 19:48:34 +0000

    Wire up url path to edit and delete function

--- project4/urls.py
+++ project4/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("subscribe/", views.subscribe, name="subscribe"),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]

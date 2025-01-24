e7a1f9a313e7a3bcfe6dcde1675a79053bde35a3
Megan Saenz <saenzmegan@ymail.com>
2025-01-24 16:40:56 +0000

    Create url patterns for delete, edit, post views

--- project4/urls.py
+++ project4/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from .views import PostCreateView
 
 urlpatterns = [
     path("subscribe/", views.subscribe, name="subscribe"),
@@ -9,4 +10,7 @@ urlpatterns = [
          views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('<slug:slug>/Edit', views.post_edit, name='post_edit'),
+    path('<slug:slug>/Delete', views.post_delete, name='post_delete'),
 ]
